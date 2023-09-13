64588c161f7047c2c0be0744e4ea28b30dc08ffe
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2023-09-13 10:55:57 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Quantas pessoas quer adicionar?");
+		int numeroPessoas=scanner.nextInt();
+		
+		//cleaner do scanner
+		scanner.nextLine();
+		
+		Pessoa[] pessoa = new Pessoa[numeroPessoas];
+		
+		for(int i=0;i<numeroPessoas;i++) {
+			pessoa[i] = new Pessoa();
+			System.out.println("Nome da pessoa "+ (i+1));
+			pessoa[i].nome=scanner.nextLine();
+		}
 
+		for(int i=0;i<numeroPessoas;i++) {
+			System.out.println("Nome da pessoa "+ (i+1) +": "+pessoa[i].nome);
+		}
 	}
 
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,13 @@
+
+public class Pessoa {
+	String nome;
+	int idade;
+	double peso;
+	
+	public void pessoa(String nome, int idade, double peso) {
+		this.nome=nome;
+		this.idade=idade;
+		this.peso=peso;
+	}
+
+}
